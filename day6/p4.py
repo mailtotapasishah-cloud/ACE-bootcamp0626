d6f14b2c414733f49ca54ce13a934b73fa057c82
mailtotapasishah-cloud <mailtotapasishah@gmail.com>
2026-06-24 06:13:21 +0000

    Add day6 work and workspace config

---
+++ day6/p4.py
@@ -0,0 +1,13 @@
+import numpy as np
+arr = np.array([1,4,5,8,9])
+mylist = [1,4,5,8,9]
+#arithmetic operations
+print(arr*2) #multipilcation
+print(arr+5) #addition
+print(arr-5) #subtraction
+print(arr/5) #division
+print(arr**2) #exponentiation
+print(arr%2) #modulus
+print(arr//2) #floor division
+   
+
